edd96b9636035ce8a55503c9e18c73cbff6af044
Christopher Mendes <bos21001@byui.edu>
2021-02-04 22:10:04 -0300

    THE RIGHT LAST ONE!!!

--- w05/js/script.js
+++ w05/js/script.js
@@ -1,7 +1,10 @@
 function convertNumbers(){
+    // INPUT = take the entered amount (float) to lbs variable
     const lbs = parseFloat(document.getElementById('lbs').value);
+    // PROCESSING = create a variable to store the conversion and the output information
     const lbs_to_kg = (parseFloat(lbs) * 0.453592).toFixed(2);
     const output = `${lbs} lbs = ${lbs_to_kg} Kg`;
+    // OUTUPUT = displays x lbs = y Kg
     document.getElementById('output').innerHTML = output;
     }
   
