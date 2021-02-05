c52d1a986bcafea9ab7d685dd1ff00449120f145
Christopher Mendes <77082167+bos21001@users.noreply.github.com>
2021-02-04 23:26:00 -0300

    Update script.js

--- w05/js/script.js
+++ w05/js/script.js
@@ -2,7 +2,7 @@ function convertNumbers(){
     // INPUT = take the entered amount (float) to lbs variable
     const lbs = parseFloat(document.getElementById('lbs').value);
     // PROCESSING = create a variable to store the conversion and the output information
-    const lbs_to_kg = (parseFloat(lbs) * 0.453592).toFixed(2);
+    const lbs_to_kg = (parseFloat(lbs) * 0.453592).toFixed(1);
     const output = `${lbs} lbs = ${lbs_to_kg} Kg`;
     // OUTUPUT = displays x lbs = y Kg
     document.getElementById('output').innerHTML = output;
