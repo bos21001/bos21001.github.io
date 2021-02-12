9e787a1f2fb2e9109eaec863fbb259530fa603bb
Christopher Mendes <bos21001@byui.edu>
2021-02-11 23:16:35 -0300

    little fix

--- w05/js/script.js
+++ w05/js/script.js
@@ -1,4 +1,4 @@
-function convertNumbers(){
+function convertNumbers() {
     // INPUT = take the entered amount (float) to lbs variable
     const lbs = parseFloat(document.getElementById('lbs').value);
     // PROCESSING = create a variable to store the conversion and the output information
@@ -6,5 +6,4 @@ function convertNumbers(){
     const output = `${lbs} lbs = ${lbsToKg} Kg`;
     // OUTUPUT = displays x lbs = y Kg
     document.getElementById('output').innerHTML = output;
-    }
-  
+}
