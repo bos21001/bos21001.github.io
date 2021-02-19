1a7f36191d923b8156460367e4134e0c43915f58
Christopher Mendes <bos21001@byui.edu>
2021-02-18 23:25:37 -0300

    Assingments under progress

---
+++ w07/alarm/js/script.js
@@ -0,0 +1,25 @@
+let now = new Date();
+let month = now.getMonth();
+let dayOfMonth = now.getDate();
+let dayOfWeek = now.getDay();
+const weekdays = (dayOfWeek >= 1 && dayOfWeek <= 5);
+
+//New Year = (month = 0, dayOfMonth = 1, (dayOfWeek >= 1 && dayOfWeek <= 5))
+//U.S Independence Day = (month = 6, dayOfMonth = 4, (dayOfWeek >= 1 && dayOfWeek <= 5))
+//Christmas = (month = 11, dayOfMonth = 24, dayOfWeek = (dayOfWeek >= 1 && dayOfWeek <= 5))
+let alarm;
+if (month == 0 && dayOfMonth == 1 && weekdays == true) {
+    alarm = "GET UP! 😝";
+}
+else if (month == 6 && dayOfMonth == 4 && weekdays == true) {
+    alarm = "GET UP! 😝";
+}
+else if (month == 11 && dayOfMonth == 24 && weekdays == true) {
+    alarm = "GET UP! 😝";
+}
+else {
+    alarm = "Sleep in! 🥱";
+}
+
+document.getElementById('actualDate').innerHTML = now;
+document.getElementById('alarm').innerHTML = alarm;
