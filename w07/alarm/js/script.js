8f0c300a2e346cd3c1c794f2f36476ed65f96554
Christopher Mendes <bos21001@byui.edu>
2021-02-19 22:09:31 -0300

    W07 ✅ Assignment: Alarm

--- w07/alarm/js/script.js
+++ w07/alarm/js/script.js
@@ -9,16 +9,16 @@ const weekdays = (dayOfWeek >= 1 && dayOfWeek <= 5);
 //Christmas = (month = 11, dayOfMonth = 24, dayOfWeek = (dayOfWeek >= 1 && dayOfWeek <= 5))
 let alarm;
 if (month == 0 && dayOfMonth == 1 && weekdays == true) {
-    alarm = "GET UP! 😝";
+    alarm = "Sleep in! 🥱";
 }
 else if (month == 6 && dayOfMonth == 4 && weekdays == true) {
-    alarm = "GET UP! 😝";
+    alarm = "Sleep in! 🥱";
 }
 else if (month == 11 && dayOfMonth == 24 && weekdays == true) {
-    alarm = "GET UP! 😝";
+    alarm = "Sleep in! 🥱";
 }
 else {
-    alarm = "Sleep in! 🥱";
+    alarm = "Get Up! 😝";
 }
 
 document.getElementById('actualDate').innerHTML = now;
